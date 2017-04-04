b0aa1f1025be82928464e4a913ec554f31221b11
CorinneEarl <corinneaearl@gmail.comgit commit --amend --reset-authorgit statusgit config --global user.email corinneaearl@gmail.com>
2017-04-04 12:49:03 -0600

    added user authentication and token information, does not include user model

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'secret': 'cohort5rocksmysocks',
+  // 'database': database
+}
--- server.js
+++ server.js
@@ -4,9 +4,72 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var config = require('./config');
 var jwt = require('jsonwebtoken');
+var User = require('./server/models/user');
 
-app.get('*', function(req, res){
-  res.json({server: 'up'})
-})
+// database.connect(config.database);
+app.set('superSecret', config.secret);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+
+//API Routes
+var apiRoutes = express.Router();
+
+//GET
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Dare-ity api launched!' });
+});
+
+//POST
+apiRoutes.post('/authenticate', function(req, res) {
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+        var token = jwt.sign(user, app.get('superSecret'), {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+        res.json({
+          success: true,
+          message: 'Please have a token!',
+          token: token
+        });
+      }
+    }
+  });
+});
+
+apiRoutes.use(function(req, res, next) {
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  if (token) {
+
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+
+  } else {
+    return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+    });
+  }
+});
+
+app.use('/api', apiRoutes);
 
 app.listen(process.env.PORT || 3001);
+console.log('magic');
