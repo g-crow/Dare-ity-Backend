a4d00545d2bf879b697e989c6e6d73eececee23a
Jacob Copley <yelpocbocaj@gmail.com>
2017-04-05 16:32:52 -0600

    working code in server file

--- server.js
+++ server.js
@@ -17,28 +17,12 @@ process.on('unhandledRejection', function(e) {
   console.log(e.message, e.stack)
 })
 
-/*app.post('/api/create_user', function(req, res){
-  const {username, user_id, hashed_password, is_npo} = req.body;
-  if(username === undefined || user_id === undefined || hashed_password === undefined || is_npo === undefined){
-    res.json(JSON.stringify("Please fill empty fields."));
-  }
-  var queryString = "INSERT INTO dareity_user (name, id, hashed_password, is_npo) "
-    + "VALUES ('" + username + "', " + user_id + ", '" + hashed_password + "', " + is_npo + ")"
-	pool.query(queryString, function(err, result){
-    if(err){
-			console.error("error", err.message);
-		} else {
-			res.json(JSON.stringify(result))
-		}
-  })
-})*/
-
 app.post('/api/create_user', function(req, res){
   const {username, hashed_password, is_npo} = req.body;
   if(username === undefined || hashed_password === undefined || is_npo === undefined){
     res.json(JSON.stringify("Please fill empty fields."));
   }
-  var queryString = "INSERT INTO dareity_user (name, hashed_password, is_npo) "
+  var queryString = "INSERT INTO client (name, hash_password, is_npo) "
     + "VALUES ('" + username + "', '" + hashed_password + "', " + is_npo + ")"
 	pool.query(queryString, function(err, result){
     if(err){
@@ -51,7 +35,7 @@ app.post('/api/create_user', function(req, res){
 
 app.post('/api/fetch_user', function(req, res){
   var username = req.body.username;
-	pool.query("SELECT id, name, is_npo FROM dareity_user WHERE name = '" + username + "'", function(err, result){
+	pool.query("SELECT id, name, is_npo FROM client WHERE name = '" + username + "'", function(err, result){
     if(err){
 			console.error("error", err.message);
 		} else {
@@ -61,12 +45,12 @@ app.post('/api/fetch_user', function(req, res){
 })
 
 app.post('/api/create_dare', function(req, res){
-  const {title, user_id, hashed_password, is_npo} = req.body;
-  if(username === undefined || user_id === undefined || hashed_password === undefined || is_npo === undefined){
+  const {dare_title, dare_description, npo_creator} = req.body;
+  if(dare_title === undefined || dare_description === undefined || npo_creator === undefined){
     res.json(JSON.stringify("Please fill empty fields."));
   }
-  var queryString = "INSERT INTO dareity_user (name, id, hashed_password, is_npo) "
-    + "VALUES ('" + username + "', " + user_id + ", '" + hashed_password + "', " + is_npo + ")"
+  var queryString = "INSERT INTO dare (dare_title, dare_description, npo_creator) "
+    + "VALUES ('" + dare_title + "', '" + dare_description + "', " + npo_creator + ")"
 	pool.query(queryString, function(err, result){
     if(err){
 			console.error("error", err.message);
@@ -78,11 +62,11 @@ app.post('/api/create_dare', function(req, res){
 
 app.post('/api/fetch_dare', function(req, res){
   var id = req.body.id;
-	pool.query("SELECT id, title, description, creator_id FROM dare WHERE id = " + id, function(err, result){
+	pool.query("SELECT id_dare, dare_title, dare_description, npo_creator, dare_expires, total_pledge_dollar_amount FROM dare WHERE id_dare = " + id, function(err, result){
     if(err){
 			console.error("error", err.message);
 		} else {
-			res.json(JSON.stringify(result))
+			res.json(JSON.stringify(result.rows[0]))
 		}
   })
 })
