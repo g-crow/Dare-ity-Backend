99f58f1dd92504c5303ffdd9b428588d0dc4e1c5
Jacob Copley <yelpocbocaj@gmail.com>
2017-04-06 15:07:32 -0600

    DB-10 branch created

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.post('/api/create_user', function(req, res){
   const {username, hashed_password, is_npo} = req.body;
   if(username === undefined || hashed_password === undefined || is_npo === undefined){
     res.json(JSON.stringify("Please fill empty fields."));
+    res.end()
   }
   var queryString = "INSERT INTO client (name, hash_password, is_npo) "
     + "VALUES ('" + username + "', '" + hashed_password + "', " + is_npo + ")"
@@ -48,6 +49,7 @@ app.post('/api/create_dare', function(req, res){
   const {dare_title, dare_description, npo_creator} = req.body;
   if(dare_title === undefined || dare_description === undefined || npo_creator === undefined){
     res.json(JSON.stringify("Please fill empty fields."))
+    res.end()
   }
   var queryString = "INSERT INTO dare (dare_title, dare_description, npo_creator) "
     + "VALUES ('" + dare_title + "', '" + dare_description + "', " + npo_creator + ")"
@@ -68,6 +70,48 @@ app.post('/api/fetch_dare', function(req, res){
 			console.error("error", err.message)
 		} else {
 			res.json(JSON.stringify(result.rows[0]))
+		}
+  })
+})
+
+app.post('/api/create_client_dare', function(req, res){
+  const {broadcaster_id, dare_id, npo_id} = req.body;
+  if(broadcaster_id === undefined || dare_id === undefined || npo_id === undefined){
+    res.json(JSON.stringify("Please fill empty fields."))
+    res.end()
+  }
+  var queryString = "INSERT INTO client_dare (broadcaster_id, dare_id, npo_id, pledge_amount_threshold) "
+    + "VALUES (" + broadcaster_id + ", " + dare_id + ", " + npo_id + ", (SELECT pledge_threshold FROM dare WHERE id_dare = " + dare_id + "))"
+	pool.query(queryString, function(err, result){
+    if(err){
+			console.error("error", err.message)
+		} else {
+			res.json(JSON.stringify(result) + "This Means Success")
+		}
+  })
+})
+
+app.post('/api/fetch_client_dare', function(req, res){
+	var id = req.body.id;
+	var queryString = "SELECT client_dare_id, broadcaster_id, dare_id, pledge_amount_threshold, npo_id, current_pledge_status FROM client_dare WHERE client_dare_id = " + id
+	pool.query(queryString, function(err, result){
+    if(err){
+			console.error("error", err.message)
+		} else {
+			res.json(JSON.stringify(result.rows[0]))
+		}
+  })
+})
+
+// one delete route for all DB records - table name, id column name, and record id must be provided
+app.post('/api/delete_record', function(req, res){
+	const {table_name, id_var, id} = req.body;
+	var queryString = "DELETE FROM " + table_name + " WHERE " + id_var + " = " + id
+	pool.query(queryString, function(err, result){
+    if(err){
+			console.error("error", err.message)
+		} else {
+			res.json(result + "This Means Success")
 		}
   })
 })
