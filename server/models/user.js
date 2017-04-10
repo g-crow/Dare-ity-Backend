ba6345b92254bb7a900566a4ddb6df9a678a3b1f
CorinneEarl <corinneaearl@gmail.comgit commit --amend --reset-authorgit statusgit config --global user.email corinneaearl@gmail.com>
2017-04-10 10:55:23 -0600

    corrected last straggling username to = name

--- server/models/user.js
+++ server/models/user.js
@@ -18,7 +18,7 @@ class User{
 			const hashed_password = ''
 			bcrypt.hash(this.password, config.saltRounds, (hashErr, hashed_password) => {
 				if (!hashErr){
-					const queryString = `INSERT INTO dareity_user (name, password, email, is_npo) VALUES ('${this.username}', '${hashed_password}', '${this.email}', ${this.is_npo})`
+					const queryString = `INSERT INTO dareity_user (name, password, email, is_npo) VALUES ('${this.name}', '${hashed_password}', '${this.email}', ${this.is_npo})`
 					db.query(queryString, callback)
 				} else {
 					callback(hashErr)
