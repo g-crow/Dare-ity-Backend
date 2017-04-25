8b2073935e49062fbe3d3588c1bb50df415a9a39
g-crow <genevieve.crow@gmail.com>
2017-04-25 09:47:08 -0600

    added image path to dare model and controller

--- server/models/dare.js
+++ server/models/dare.js
@@ -8,14 +8,14 @@ class Dare{
 		this.npo_creator = npo_creator;
 		this.expiration = expiration;
 		this.pledge_threshold = pledge_threshold;
-		this.video_path = video_path;
+		this.image_path = image_path;
 	}
 
 	save(callback){
-		if (!this.title || !this.description || !this.expiration || !this.pledge_threshold){
-			callback(new Error('Please make sure title, description, expiration, and the pledge threshold are entered.'))
+		if (!this.title || !this.description) {
+			callback(new Error('Please make sure both title and description are entered.'))
 		} else {
-			const queryString = `INSERT INTO dare (title, description, npo_creator, expiration, pledge_threshold) VALUES ('${this.title}', '${this.description}', ${this.npo_creator}, '${this.expiration}', ${this.pledge_threshold})`
+			const queryString = `INSERT INTO dare (title, description, npo_creator, expiration, pledge_threshold, image_path) VALUES ('${this.title}', '${this.description}', ${this.npo_creator}, '${this.expiration}', ${this.pledge_threshold}, '${this.image_path}')`
 			db.query(queryString, function(err, result) {
 				console.log(err);
 			    if (err) {
