a69d4e0f06d55763aaa4eb381c557ca504a871c8
Liza Sautter <lizasautter@gmail.com>
2017-04-26 14:33:44 -0600

    modified pledge route so it doesnt crash the server

--- server/models/pledge.js
+++ server/models/pledge.js
@@ -14,16 +14,17 @@ class Pledge {
   save(callback){
     if (!this.pledger_id || !this.broadcaster_id || !this.dare_id || !this.npo_id || !this.user_dare_id || !this.pledge_amount || !this.to_refund) {
       callback(new Error('Please provide the correct information.'))
+    } else {
+      const queryString = `INSERT INTO pledge (pledger_id, broadcaster_id, dare_id, npo_id, user_dare_id, pledge_amount, to_refund) VALUES (${this.pledger_id}, ${this.broadcaster_id}, ${this.dare_id}, ${this.npo_id}, ${this.user_dare_id}, ${this.pledge_amount}, ${this.to_refund})`
+      db.query(queryString, function(err, result) {
+        if (err) {
+          console.error('error', err.message)
+          callback(err.message)
+        } else {
+          callback(null, result)
+        }
+   	  })
     }
-    const queryString = `INSERT INTO pledge (pledger_id, broadcaster_id, dare_id, npo_id, user_dare_id, pledge_amount, to_refund) VALUES (${this.pledger_id}, ${this.broadcaster_id}, ${this.dare_id}, ${this.npo_id}, ${this.user_dare_id}, ${this.pledge_amount}, ${this.to_refund})`
-    db.query(queryString, function(err, result) {
-      if (err) {
-        console.error('error', err.message)
-        callback(err.message)
-      } else {
-        callback(null, result)
-      }
- 	  })
   }
 }
 
