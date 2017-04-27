3d1f83654ffc3b8bd25da5c8992a538538ff3699
Liza Sautter <lizasautter@gmail.com>
2017-04-27 10:57:06 -0600

    fetch all user update

--- server/models/user.js
+++ server/models/user.js
@@ -92,7 +92,7 @@ User.fetchUser = function(query, callback) {
 
 User.fetchAllUsers = function(query, callback) {
   db.query(
-		`SELECT dareity_user.id AS user_id,
+		`SELECT 						dareity_user.id AS user_id,
 						dareity_user.is_npo,
 						dareity_user.name,
 						dareity_user.email,
@@ -106,11 +106,15 @@ User.fetchAllUsers = function(query, callback) {
 						user_dare.video_path,
 						user_dare.pledge_amount_threshold,
 						npo.name AS npo_name,
-						npo.id AS npo_id
-		 FROM dareity_user
+						npo.id AS npo_id,
+						pledge_totals.total_pledges
+
+			FROM dareity_user
 				LEFT JOIN user_dare ON dareity_user.id = user_dare.broadcaster_id
 				LEFT JOIN dare ON user_dare.dare_id = dare.id
-				LEFT JOIN dareity_user AS npo ON dare.npo_creator = npo.id`,
+				LEFT JOIN dareity_user AS npo ON dare.npo_creator = npo.id
+				LEFT JOIN (SELECT user_dare_id, sum(pledge_amount) as total_pledges FROM pledge GROUP BY user_dare_id) AS pledge_totals ON user_dare.id = pledge_totals.user_dare_id
+			`,
 		function(err, result){
 		    const rows = (result.rows)
 				console.log('rows', rows)
@@ -129,7 +133,9 @@ User.fetchAllUsers = function(query, callback) {
 																pledge_amount_threshold: row.pledge_amount_threshold,
 																description: row.description,
 																image_path: row.image_path,
-																npo_name: row.npo_name
+																npo_name: row.npo_name,
+																user_dare_id: row.user_dare_id,
+																total_pledges: total_pledges
 															}))
 					const users = rows.reduce((users, row) => {
 						users[row.name] = {
